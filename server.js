62c5d71a54e35453f26ca0d1338b001a1d2b8d57
Miagotobene <miagotobene@gmail.com>
2024-05-27 22:22:43 -0400

    feat: created routes, ejs files and used bootstrap for styling

---
+++ server.js
@@ -0,0 +1,43 @@
+// Import modules
+const express = require('express');
+const app = express();
+const path = require('path');
+const ejs = require('ejs');
+const port = 3000;
+
+// serve static files
+app.use(express.static('public'));
+
+// use templating engines
+app.set('view engine', 'ejs')
+
+// set up the homepage route and serve index.html
+app.get('/', (req, res) => {
+    res.render('index');
+
+})
+
+// set up the about page route and serve about.html
+app.get('/about', (req, res) => {
+    res.render('about');
+})
+
+// set up the overview page route and serve overview.html
+app.get('/contact', (req, res) => {
+    res.render('contact');
+
+})
+
+// set up the sign up page route and serve signup.html
+app.get('/login', (req, res) => {
+    res.render('login');
+
+})
+
+
+
+// Server
+app.listen(port, () => {
+    console.log(`App's running on PORT: ${port}`)
+}
+)
